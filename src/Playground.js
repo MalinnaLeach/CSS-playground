aea6540e73e55c1a864feb50cd9fcbe11030a05e
Blanca Mendizabal Perello <blanca.mendizabal.perello@gmail.com>
2016-12-06 16:55:23 +0000

    Make playground clickable

--- src/Playground.js
+++ src/Playground.js
@@ -1,18 +1,27 @@
 import React, { Component } from 'react';
+import Popup from 'react-popup';
 
 class Playground extends Component {
   constructor(props) {
     super(props);
+    this.state = { test: "" };
+    this.createDiv = this.createDiv.bind(this);
+  }
 
+  createDiv() {
+    // Popup.alert('Click anywhere to start!');
+    this.setState({ test: "Click here"});
   }
 
   render () {
     return (
-      <div className="playground">
-      Playground goes here
+      <div className="playground" onClick={this.createDiv}>
+        Playground goes here
       </div>
     );
   }
+
+
 }
 
 export default Playground;
---
+++ tests/Playground.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
+import Playground from '../src/Playground';
+
+
+describe('<Playground />', () => {
+
+  it('simulates click events', () => {
+    
+  });
+
+});
