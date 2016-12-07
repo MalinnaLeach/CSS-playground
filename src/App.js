9a375ade3e2f275c7e070a8754c07f02d33848f7
Malinna Leach <malinnab@yahoo.com>
2016-12-07 17:48:10 +0000

    container turns blue

--- src/App.js
+++ src/App.js
@@ -7,24 +7,27 @@ import '../public/css/App.css';
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      divs: 0
-    };
+    this.state = {divs: 0, color: "red"};
     this.addChildDiv = this.addChildDiv.bind(this)
+    this.changeColor = this.changeColor.bind(this)
   }
 
   render () {
   return (
     <div className="App">
       <Header />
-      <Playground divCount = {this.state.divs}/>
-      <ControlPanel addChildDiv = {this.addChildDiv}/>
+      <Playground divCount = {this.state.divs} color = {this.state.color}/>
+      <ControlPanel addChildDiv = {this.addChildDiv} changeColor = {this.changeColor}/>
     </div>
     );
   };
 
   addChildDiv() {
     this.setState({ divs: 1});
+  }
+
+  changeColor() {
+    this.setState({color: "blue"})
   }
 }
 
