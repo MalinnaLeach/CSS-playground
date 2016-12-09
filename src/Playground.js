ee7e12b64508a77ba0ddd949f6ab177473bff937
Julio Gonzalez <julio.gteran@gmail.com>
2016-12-09 10:16:31 +0000

    Color menu working in the playground

--- src/Playground.js
+++ src/Playground.js
@@ -1,24 +1,47 @@
 import React, { Component } from 'react';
 import Popup from 'react-popup';
 import Container from './Container';
+import Menu from '../src/Menu'
 import '../public/css/Playground.css';
 
 class Playground extends Component {
   constructor(props) {
     super(props);
-    this.state = {}
-    this.checkDiv = this.checkDiv.bind(this);
+    this.state = {color: "white", divs: []}
+    this.choices = this.choices.bind(this);
+    this.onDrag = this.onDrag.bind(this);
+    this.addChildDiv = this.addChildDiv.bind(this);
+    this.renderDiv = this.renderDiv.bind(this);
   }
 
-  checkDiv() {
-    if (this.props.divCount === 1) {return <Container color={this.props.color}/>};
+  renderDiv() {
+    return this.state.divs.map(div=> (
+      <Container color={this.state.color} />
+    ))
+  }
+
+  addChildDiv() {
+    this.setState({ divs: [...this.state.divs, "div"]});
+  }
+
+  choices() {
+    const here = this
+    Popup.create({
+      content: <Menu value={here.state.color} onDrag={here.onDrag} addChildDiv={here.addChildDiv}/>
+    })
+  }
+
+  onDrag(color) {
+    this.setState({
+      color: color
+    })
   }
 
   render () {
     return (
-      <div className="playground" >
+      <div className="playground" onClick={ this.choices } style={{backgroundColor: this.state.color}}>
         Playground goes here
-        {this.checkDiv()}
+        {this.renderDiv()}
       </div>
     );
   }
