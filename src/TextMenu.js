b4d603cb3aca50f65d945119bded9d3b53bbe4eb
Malinna Leach <malinnab@yahoo.com>
2016-12-13 16:39:49 +0000

    changeable font-sizes

--- src/TextMenu.js
+++ src/TextMenu.js
@@ -7,10 +7,16 @@ class TextMenu extends Component {
     this.handleText = this.handleText.bind(this);
     this.placeholderCheck = this.placeholderCheck.bind(this);
     this.valueCheck = this.valueCheck.bind(this);
+    this.handleFontSize = this.handleFontSize.bind(this);
   }
 
-  handleText (e) {
+  handleText(e) {
     this.props.updateText(e.target.value);
+  }
+
+  handleFontSize(e) {
+    this.setState({fontSize: e});
+    this.props.changeFontSize(e.target.value);
   }
 
   valueCheck() {
@@ -18,6 +24,14 @@ class TextMenu extends Component {
       return null
   } else {
       return this.props.content
+    }
+  }
+
+  sizeCheck() {
+    if (!!this.props.fontSize) {
+      return this.props.fontSize.split("px")[0]
+  } else {
+      return null
     }
   }
 
@@ -29,11 +43,21 @@ class TextMenu extends Component {
     }
   }
 
+  sizePlaceCheck() {
+    if (!!this.props.fontSize) {
+      return null
+  } else {
+      return "Input font size in pixels"
+    }
+  }
+
   render () {
     return (
       <div className="textInput">
         <p>Your text: </p>
-        <input id="textInput" type="text" placeholder={this.placeholderCheck()} defaultValue={this.valueCheck()} onChange={this.handleText}/>
+        <input id="textInput" type="text" placeholder={this.placeholderCheck()} defaultValue={this.valueCheck()} onChange={this.handleText} />
+        <p>Change font size: </p>
+        <input id="textInput" type="text" placeholder={this.sizePlaceCheck()} defaultValue={this.sizeCheck()} onChange={this.handleFontSize} /> px
         <Positioning changeAlignment={this.props.changeAlignment} increaseLeftMargin={this.props.increaseLeftMargin} decreaseLeftMargin = {this.props.decreaseLeftMargin}
         increaseRightMargin={this.props.increaseRightMargin} decreaseRightMargin={this.props.decreaseRightMargin}
         increaseTopMargin={this.props.increaseTopMargin} decreaseTopMargin = {this.props.decreaseTopMargin}
