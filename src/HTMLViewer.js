62d3795acc76b5473ed1fc458c39c97159b4a014
Blanca Mendizabal Perello <blanca.mendizabal.perello@gmail.com>
2016-12-13 18:23:12 +0000

    Remove extra spaces in HTML Viewer

--- src/HTMLViewer.js
+++ src/HTMLViewer.js
@@ -9,15 +9,15 @@ class HTMLViewer extends Component {
   render () {
     return (
       <div className="HTMLViewer" style={{textAlign: "left"}}>
-      <p> &#60; &#33;DOCTYPE html &#62;</p>
-      <p> &#60; html &#62;</p>
-      <p> &nbsp;&nbsp; &#60;head &#62;</p>
-      <p> &nbsp;&nbsp; &nbsp;&nbsp; &#60;meta charset="utf-8" &#62;</p>
-      <p> &nbsp;&nbsp; &nbsp;&nbsp; &#60;title &#62; &#60;/title &#62;</p>
-      <p> &nbsp;&nbsp; &#60;/head &#62;</p>
-      <p> &nbsp;&nbsp; &#60;body &#62;</p>
-      <p> &nbsp;&nbsp; &#60;/body &#62;</p>
-      <p> &#60; /html &#62;</p>
+      <p> &#60;&#33;DOCTYPE html&#62;</p>
+      <p> &#60;html&#62;</p>
+      <p> &nbsp;&nbsp; &#60;head&#62;</p>
+      <p> &nbsp;&nbsp; &nbsp;&nbsp; &#60;meta charset="utf-8"&#62;</p>
+      <p> &nbsp;&nbsp; &nbsp;&nbsp; &#60;title&#62; &#60;/title&#62;</p>
+      <p> &nbsp;&nbsp; &#60;/head&#62;</p>
+      <p> &nbsp;&nbsp; &#60;body&#62;</p>
+      <p> &nbsp;&nbsp; &#60;/body&#62;</p>
+      <p> &#60;/html&#62;</p>
       </div>
     );
   }
