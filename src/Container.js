cc3b92ce679b642882711f7c6c2222c0068ee1d9
Julio Gonzalez <julio.gteran@gmail.com>
2016-12-14 10:47:24 +0000

    images

--- src/Container.js
+++ src/Container.js
@@ -88,10 +88,7 @@ class Container extends Component {
   }
 
   addChildImage(url) {
-    console.log(url)
-    this.state.style["background-image"] = url
     this.setState({images: [...this.state.images, url]});
-    this.props.updateCssViewer()
   }
 
   addChildText(textType) {
