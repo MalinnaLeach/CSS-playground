cc03a124c54cbfc2489f58a99ea60be163452b38
Blanca Mendizabal Perello <blanca.mendizabal.perello@gmail.com>
2016-12-14 12:45:15 +0000

    Add children divs inside parent divs

--- src/HTMLTranslator.js
+++ src/HTMLTranslator.js
@@ -2,26 +2,39 @@ import React, { Component } from 'react';
 import htmlModule from './htmlModule';
 
 class HTMLTranslator extends Component {
- constructor(props) {
-   super(props);
+  constructor(props) {
+    super(props);
+  }
 
- }
+  htmlOpen(element) {
+  return (
+    <p>&nbsp;&nbsp;&nbsp;&nbsp;&#60;{element.type}&nbsp;class={element.class}&#62;</p>
+  )
+  }
 
- htmlLine() {
-   return htmlModule.map(element => (
-     <div>
-       <p>&nbsp;&nbsp;&nbsp;&nbsp;&#60;{element.type}&nbsp;class={element.class}&#62;</p>
-       <p>&nbsp;&nbsp;&nbsp;&nbsp;&#60;&#47;{element.type}&#62;</p>
-     </div>
-   ))
- }
+  htmlClose(element) {
+    return (
+      <p>&nbsp;&nbsp;&nbsp;&nbsp;&#60;&#47;{element.type}&#62;</p>
+    )
+  }
 
- render () {
-   return (
-     <div>
-       {this.htmlLine()}
-     </div>
- )}
+  renderHTML(array, results = []) {
+    for (var element of array) {
+      results.push(this.htmlOpen(element))
+      if (element.children !== []) {
+        this.renderHTML(element.children, results)
+      }
+      results.push(this.htmlClose(element))
+    }
+    return results
+  }
+
+  render () {
+    return (
+      <div>
+        {this.renderHTML(htmlModule[0].children)}
+      </div>
+  )}
 }
 
 export default HTMLTranslator;
