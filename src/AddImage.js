36f566be734dcfcbf1c78299ed24ee729c0e77c9
Julio Gonzalez <julio.gteran@gmail.com>
2016-12-16 09:54:45 +0000

    Testing and refactoring

--- src/AddImage.js
+++ src/AddImage.js
@@ -8,20 +8,13 @@ import htmlModule from './htmlModule'
 class AddImage extends Component {
   constructor(props) {
     super(props);
-
+    this.updateCssModule = this.updateCssModule.bind(this)
     this.state = {imageUrl: props.imageUrl, style: {height: props.height}}
     this.updateHeight = this.updateHeight.bind(this)
     this.showImageMenu = this.showImageMenu.bind(this)
     this.setHeight = this.setHeight.bind(this)
     this.changeAlignment = this.changeAlignment.bind(this);
-    this.increaseLeftMargin = this.increaseLeftMargin.bind(this);
-    this.decreaseLeftMargin = this.decreaseLeftMargin.bind(this);
-    this.increaseRightMargin = this.increaseRightMargin.bind(this);
-    this.decreaseRightMargin = this.decreaseRightMargin.bind(this);
-    this.increaseTopMargin = this.increaseTopMargin.bind(this);
-    this.decreaseTopMargin = this.decreaseTopMargin.bind(this);
-    this.increaseBottomMargin = this.increaseBottomMargin.bind(this);
-    this.decreaseBottomMargin = this.decreaseBottomMargin.bind(this);
+    this.changeMargin = this.changeMargin.bind(this);
   }
 
   setHeight() {
@@ -37,9 +30,7 @@ class AddImage extends Component {
   showImageMenu (e) {
     const here = this
     Popup.create({
-      content: <ImageMenu updateHeight={here.updateHeight} height={here.setHeight()} changeAlignment={here.changeAlignment} increaseLeftMargin={here.increaseLeftMargin} decreaseLeftMargin={here.decreaseLeftMargin} increaseRightMargin={here.increaseRightMargin} decreaseRightMargin={here.decreaseRightMargin}
-      increaseTopMargin={here.increaseTopMargin} decreaseTopMargin={here.decreaseTopMargin} increaseBottomMargin={here.increaseBottomMargin}
-      decreaseBottomMargin={here.decreaseBottomMargin} />,
+      content: <ImageMenu updateHeight={here.updateHeight} height={here.setHeight()} changeAlignment={here.changeAlignment} changeMargin={here.changeMargin}/>,
       buttons: {right:['ok']}
     })
     if (!e) var e = window.event;
@@ -57,90 +48,13 @@ class AddImage extends Component {
     this.props.updateCssViewer();
   }
 
-  increaseLeftMargin() {
-    if (!!this.state.style["marginLeft"]) {
-      var margin = parseInt((this.state.style["marginLeft"].split("%"))[0]);
-      this.state.style["marginLeft"] = String((margin + 5)) + "%"
+  changeMargin(size, dimension) {
+    if (!!this.state.style["margin" + dimension]) {
+      var margin = parseInt((this.state.style["margin" + dimension].split("%"))[0]);
+      this.state.style["margin" + dimension] = String((margin + size)) + "%"
       this.props.updateCssViewer();
     } else {
-      this.state.style["marginLeft"] = "5%"
-      this.props.updateCssViewer();
-    }
-  }
-
-  decreaseLeftMargin() {
-    if (!!this.state.style["marginLeft"]) {
-      var margin = parseInt((this.state.style["marginLeft"].split("%"))[0]);
-      this.state.style["marginLeft"] = String((margin - 5)) + "%"
-      this.props.updateCssViewer();
-    } else {
-      this.state.style["marginLeft"] = "-5%"
-      this.props.updateCssViewer();
-    }
-  }
-
-  increaseRightMargin() {
-    if (!!this.state.style["marginRight"]) {
-      var margin = parseInt((this.state.style["marginRight"].split("%"))[0]);
-      this.state.style["marginRight"] = String((margin + 5)) + "%"
-      this.props.updateCssViewer();
-    } else {
-      this.state.style["marginRight"] = "5%"
-      this.props.updateCssViewer();
-    }
-  }
-
-  decreaseRightMargin() {
-    if (!!this.state.style["marginRight"]) {
-      var margin = parseInt((this.state.style["marginRight"].split("%"))[0]);
-      this.state.style["marginRight"] = String((margin - 5)) + "%"
-      this.props.updateCssViewer();
-    } else {
-      this.state.style["marginRight"] = "-5%"
-      this.props.updateCssViewer();
-    }
-  }
-
-  increaseTopMargin() {
-    if (!!this.state.style["marginTop"]) {
-      var margin = parseInt((this.state.style["marginTop"].split("%"))[0]);
-      this.state.style["marginTop"] = String((margin + 5)) + "%"
-      this.props.updateCssViewer();
-    } else {
-      this.state.style["marginTop"] = "5%"
-      this.props.updateCssViewer();
-    }
-  }
-
-  decreaseTopMargin() {
-    if (!!this.state.style["marginTop"]) {
-      var margin = parseInt((this.state.style["marginTop"].split("%"))[0]);
-      this.state.style["marginTop"] = String((margin - 5)) + "%"
-      this.props.updateCssViewer();
-    } else {
-      this.state.style["marginTop"] = "-5%"
-      this.props.updateCssViewer();
-    }
-  }
-
-  increaseBottomMargin() {
-    if (!!this.state.style["marginBottom"]) {
-      var margin = parseInt((this.state.style["marginBottom"].split("%"))[0]);
-      this.state.style["marginBottom"] = String((margin + 5)) + "%"
-      this.props.updateCssViewer();
-    } else {
-      this.state.style["marginBottom"] = "5%"
-      this.props.updateCssViewer();
-    }
-  }
-
-  decreaseBottomMargin() {
-    if (!!this.state.style["marginBottom"]) {
-      var margin = parseInt((this.state.style["marginBottom"].split("%"))[0]);
-      this.state.style["marginBottom"] = String((margin - 5)) + "%"
-      this.props.updateCssViewer();
-    } else {
-      this.state.style["marginBottom"] = "-5%"
+      this.state.style["margin" + dimension] = String(size) + "%"
       this.props.updateCssViewer();
     }
   }
@@ -166,7 +80,6 @@ class AddImage extends Component {
   }
 
   render () {
-    console.log(this.state.style.height)
     return (
       <div className="AddImage" onClick={this.showImageMenu}>
         <img src={this.state.imageUrl} style={this.state.style} />
--- src/ImageMenu.js
+++ src/ImageMenu.js
@@ -6,16 +6,56 @@ class ImageMenu extends Component {
     super(props);
     this.handleHeight = this.handleHeight.bind(this);
     this.heightCheck = this.heightCheck.bind(this);
+    this.increaseLeftMargin = this.increaseLeftMargin.bind(this);
+    this.decreaseLeftMargin = this.decreaseLeftMargin.bind(this);
+    this.increaseRightMargin = this.increaseRightMargin.bind(this);
+    this.decreaseRightMargin = this.decreaseRightMargin.bind(this);
+    this.increaseTopMargin = this.increaseTopMargin.bind(this);
+    this.decreaseTopMargin = this.decreaseTopMargin.bind(this);
+    this.increaseBottomMargin = this.increaseBottomMargin.bind(this);
+    this.decreaseBottomMargin = this.decreaseBottomMargin.bind(this);
   }
 
   handleHeight (e) {
     this.props.updateHeight(e.target.value);
   }
 
-
   heightCheck() {
     return this.props.height.split("vh")[0]
   }
+
+  increaseLeftMargin() {
+    this.props.changeMargin(5, "Left")
+  }
+
+  decreaseLeftMargin() {
+    this.props.changeMargin(-5, "Left")
+  }
+
+  increaseRightMargin() {
+    this.props.changeMargin(5, "Right")
+  }
+
+  decreaseRightMargin() {
+    this.props.changeMargin(-5, "Right")
+  }
+
+  increaseTopMargin() {
+    this.props.changeMargin(5, "Top")
+  }
+
+  decreaseTopMargin() {
+    this.props.changeMargin(-5, "Top")
+  }
+
+  increaseBottomMargin() {
+    this.props.changeMargin(5, "Bottom")
+  }
+
+  decreaseBottomMargin() {
+    this.props.changeMargin(-5, "Bottom")
+  }
+
 
 
   render () {
@@ -23,10 +63,10 @@ class ImageMenu extends Component {
       <div className="textInput">
         <p>Change image size: </p>
         <input id="imageInput" type="text" defaultValue={this.heightCheck()} onChange={this.handleHeight}/>vh
-        <Positioning changeAlignment={this.props.changeAlignment} increaseLeftMargin={this.props.increaseLeftMargin} decreaseLeftMargin = {this.props.decreaseLeftMargin}
-        increaseRightMargin={this.props.increaseRightMargin} decreaseRightMargin={this.props.decreaseRightMargin}
-        increaseTopMargin={this.props.increaseTopMargin} decreaseTopMargin = {this.props.decreaseTopMargin}
-        increaseBottomMargin={this.props.increaseBottomMargin} decreaseBottomMargin = {this.props.decreaseBottomMargin} />
+        <Positioning changeAlignment={this.props.changeAlignment} increaseLeftMargin={this.increaseLeftMargin} decreaseLeftMargin = {this.decreaseLeftMargin}
+        increaseRightMargin={this.increaseRightMargin} decreaseRightMargin={this.decreaseRightMargin}
+        increaseTopMargin={this.increaseTopMargin} decreaseTopMargin = {this.decreaseTopMargin}
+        increaseBottomMargin={this.increaseBottomMargin} decreaseBottomMargin = {this.decreaseBottomMargin} />
       </div>
     )
   }
